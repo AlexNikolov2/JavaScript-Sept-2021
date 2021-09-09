ffcfee1bd3895b853ff8c5594e1c3b2054d741d1
Aleks Nikolov <tcr.official.world@gmail.com>
2021-09-09 20:55:16 +0300

    app is fully ready noww

--- JS-global-clock/app.js
+++ JS-global-clock/app.js
@@ -5,45 +5,36 @@
 
   function clock() {
     function londonClock() {
-      let londonOffset = "0";
-      let londonClock = d.getTime() + d.getTimezoneOffset() * 60000;
-      let ndLondon = new Date(londonClock + 3600000 * londonOffset);
       let londonDateEl = document.createElement("p");
       londonDateEl.classList.add("london-date");
-      londonDateEl.innerHTML = ndLondon.toLocaleDateString("en");
+      londonDateEl.innerHTML = d.toLocaleDateString("en", {timeZone: "Europe/London"});
       let londonTimeEl = document.createElement("p");
       londonTimeEl.classList.add("london-time");
-      londonTimeEl.innerHTML = ndLondon.toLocaleTimeString("en")
+      londonTimeEl.innerHTML = d.toLocaleTimeString("en", {timeZone: "Europe/London"})
       london.appendChild(londonDateEl);
       london.appendChild(londonTimeEl);
     }
   londonClock()
   
     function beijingClock() {
-      let beijingOffset = "9";
-      let beijingClock = d.getTime() + d.getTimezoneOffset() * 60000;
-      let ndbeijing = new Date(beijingClock + 3600000 * beijingOffset);
       let beijingDateEl = document.createElement("p");
       beijingDateEl.classList.add("beijing-date");
-      beijingDateEl.innerHTML = ndbeijing.toLocaleDateString("en");
+      beijingDateEl.innerHTML = d.toLocaleDateString("en", {timeZone:   'Asia/Shanghai'    });
       let beijingTimeEl = document.createElement("p");
       beijingTimeEl.classList.add("beijing-time");
-      beijingTimeEl.innerHTML = ndbeijing.toLocaleTimeString('en')
+      beijingTimeEl.innerHTML = d.toLocaleTimeString('en', {timeZone: 'Asia/Shanghai'})
       beijing.appendChild(beijingDateEl);
       beijing.appendChild(beijingTimeEl);
     }
   beijingClock()
   
     function sofiaClock() {
-      let sofiaOffset = "2";
-      let sofiaClock = d.getTime() + d.getTimezoneOffset() * 60000;
-      let ndsofia = new Date(sofiaClock + 3600000 * sofiaOffset);
       let sofiaDateEl = document.createElement("p");
       sofiaDateEl.classList.add("sofia-date");
-      sofiaDateEl.innerHTML = ndsofia.toLocaleDateString("en");
+      sofiaDateEl.innerHTML = d.toLocaleDateString("en", {timeZone: "Europe/Sofia"});
       let sofiaTimeEl = document.createElement("p");
       sofiaTimeEl.classList.add("sofia-time");
-      sofiaTimeEl.innerHTML = ndsofia.toLocaleTimeString("en")
+      sofiaTimeEl.innerHTML = d.toLocaleTimeString("en", {timeZone:"Europe/Sofia"})
       sofia.appendChild(sofiaDateEl);
       sofia.appendChild(sofiaTimeEl);
     }  
