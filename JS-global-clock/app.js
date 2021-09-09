c5685f9a1875e8cd607252a656acb1563eac5c69
Aleks Nikolov <tcr.official.world@gmail.com>
2021-09-09 20:45:15 +0300

    commit 9/9, app is ready!

--- JS-global-clock/app.js
+++ JS-global-clock/app.js
@@ -1,41 +1,52 @@
-function time() {
-  let dateLondonEl = document.querySelector(".date-london");
-  let timeLondonEl = document.querySelector(".time-london");
-  let dateSofiaEl = document.querySelector(".date-sofia");
-  let timeSofiaEl = document.querySelector(".time-sofia");
-  let dateBeijingEl = document.querySelector(".date-beijing");
-  let timeBeijingEl = document.querySelector(".time-beijing");
+  let london = document.getElementById("london-clock");
+  let sofia = document.getElementById("sofia-clock");
+  let beijing = document.getElementById("beijing-clock");
+  let d = new Date();
 
-  let londonDate = new Date().toLocaleDateString("en-US", {
-    timeZone: "Europe/London",
-    hourCycle: "h23",
-  });
-  let londonTime = new Date().toLocaleTimeString("en-US", {
-    timeZone: "Europe/London",
-    hourCycle: "h23",
-  });
-  dateLondonEl.innerHTML = londonDate;
-  timeLondonEl.innerHTML = londonTime;
-
-  let beijingDate = new Date().toLocaleDateString("en-US", {
-    timeZone: "Asia/Beijing",
-    hourCycle: "h23",
-  });
-  let beijingTime = new Date().toLocaleTimeString("en-US", {
-    timeZone: "Asia/Beijing",
-    hourCycle: "h23",
-  });
-  dateBeijingEl.innerHTML = beijingDate;
-  timeBeijingEl.innerHTML = beijingTime;
-
-  let sofiaDate = new Date().toLocaleDateString("en-US", {
-    timeZone: "Europe/Sofia",
-    hourCycle: "h23",
-  });
-  let sofiaTime = new Date().toLocaleTimeString("en-US", {
-    timeZone: "Europe/Sofia",
-    hourCycle: "h23",
-  });
-  dateSofiaEl.innerHTML = sofiaDate;
-  timeSofiaEl.innerHTML = sofiaTime;
-}
+  function clock() {
+    function londonClock() {
+      let londonOffset = "0";
+      let londonClock = d.getTime() + d.getTimezoneOffset() * 60000;
+      let ndLondon = new Date(londonClock + 3600000 * londonOffset);
+      let londonDateEl = document.createElement("p");
+      londonDateEl.classList.add("london-date");
+      londonDateEl.innerHTML = ndLondon.toLocaleDateString("en");
+      let londonTimeEl = document.createElement("p");
+      londonTimeEl.classList.add("london-time");
+      londonTimeEl.innerHTML = ndLondon.toLocaleTimeString("en")
+      london.appendChild(londonDateEl);
+      london.appendChild(londonTimeEl);
+    }
+  londonClock()
+  
+    function beijingClock() {
+      let beijingOffset = "9";
+      let beijingClock = d.getTime() + d.getTimezoneOffset() * 60000;
+      let ndbeijing = new Date(beijingClock + 3600000 * beijingOffset);
+      let beijingDateEl = document.createElement("p");
+      beijingDateEl.classList.add("beijing-date");
+      beijingDateEl.innerHTML = ndbeijing.toLocaleDateString("en");
+      let beijingTimeEl = document.createElement("p");
+      beijingTimeEl.classList.add("beijing-time");
+      beijingTimeEl.innerHTML = ndbeijing.toLocaleTimeString('en')
+      beijing.appendChild(beijingDateEl);
+      beijing.appendChild(beijingTimeEl);
+    }
+  beijingClock()
+  
+    function sofiaClock() {
+      let sofiaOffset = "2";
+      let sofiaClock = d.getTime() + d.getTimezoneOffset() * 60000;
+      let ndsofia = new Date(sofiaClock + 3600000 * sofiaOffset);
+      let sofiaDateEl = document.createElement("p");
+      sofiaDateEl.classList.add("sofia-date");
+      sofiaDateEl.innerHTML = ndsofia.toLocaleDateString("en");
+      let sofiaTimeEl = document.createElement("p");
+      sofiaTimeEl.classList.add("sofia-time");
+      sofiaTimeEl.innerHTML = ndsofia.toLocaleTimeString("en")
+      sofia.appendChild(sofiaDateEl);
+      sofia.appendChild(sofiaTimeEl);
+    }  
+    sofiaClock()
+  }
+  clock();
