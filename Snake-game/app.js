e8664eb62c525af5c2940174fcff28c6e5425761
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2021-10-15 21:29:49 +0300

    cummit

--- Snake-game/app.js
+++ Snake-game/app.js
@@ -29,12 +29,50 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function moveOutcomes(){
+        if (
+            (currentSnake[0] + width >= (width * width) && direction === width ) || 
+            (currentSnake[0] % width === width -1 && direction === 1) ||
+            (currentSnake[0] % width === 0 && direction === -1) || 
+            (currentSnake[0] - width < 0 && direction === -width) || 
+            squares[currentSnake[0] + direction].classList.contains('snake')
+          ) {
+            return clearInterval(interval)
+          }
+      
+          const tail = currentSnake.pop(); 
+          squares[tail].classList.remove('snake');
+          currentSnake.unshift(currentSnake[0] + direction);
 
+          if(squares[currentSnake[0]].classList.contains('apple')) {
+            squares[currentSnake[0]].classList.remove('apple');
+            squares[tail].classList.add('snake');
+            currentSnake.push(tail);
+            randomApple();
+            score++;
+            scoreDisplay.textContent = score;
+            clearInterval(interval);
+            intervalTime = intervalTime * speed;
+            interval = setInterval(moveOutcomes, intervalTime);
+          }
+          squares[currentSnake[0]].classList.add('snake');
     }
     function randomApple(){
 
     }
-    function control(){
-        
-    }
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
+    
+        if(e.keyCode === 39) {
+          direction = 1;
+        } else if (e.keyCode === 38) {
+          direction = -width ;
+        } else if (e.keyCode === 37) {
+          direction = -1;
+        } else if (e.keyCode === 40) {
+          direction = +width;
+        }
+      }
+    
+      document.addEventListener('keyup', control);
+      startButton.addEventListener('click', startGame);
 })
